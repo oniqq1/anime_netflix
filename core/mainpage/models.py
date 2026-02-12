374929cd8d6fc20296c1b7b5a71c47882560e7e5
mailor <vvorlovv7799@gmailc.om>
2026-02-12 18:48:24 +0200

    Добавил так сильно нужную нам модель. Избавил нас от так ненавистного нам хардкода

--- core/mainpage/models.py
+++ core/mainpage/models.py
@@ -2,14 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Comment(models.Model):
-    author = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
-    comment_content = models.TextField(max_length="455")
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    # Крутая штука кстати
-    class Meta:
-        ordering = ['-created_at']
+class AnimeDescription(models.Model):
+    name = models.CharField()
+    appearing = models.TextField()
+    type = models.TextField()
+    genres = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
-        return self.author.username
+        return self.name
--- core/mainpage/views.py
+++ core/mainpage/views.py
@@ -1,9 +1,37 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import AnimeDescription
 
 
-def steins_gate_page(request: HttpRequest) -> HttpResponse:
-    return render(request, "steins-gate_page.html")
+def steins_gate_page(request):
+    anime, created = AnimeDescription.objects.get_or_create(
+        name="Steins;Gate",
+        defaults={
+            "appearing": "2011 весна",
+            "type": "ТВ (25 эп.), 25 мин.",
+            "genres": "Триллер, Фантастика, Научная фантастика, Тайна, Психологический хоррор, Приключение, Романтика",
+            "description": """В Вратах Штейна рассказывается о группе молодых студентов-технарей.
+        Студенты находят способ менять прошлое по почте с помощью модифицированной микроволновки и начинают опыты с целью узнать,
+        насколько далеко сможет зайти их открытие, но в итоге все начинает выходить из-под контроля,
+        и студенты впутываются в заговор вокруг SERN — организации, стоящей за Большим адронным коллайдером — и Джона Титора,
+        утверждающего, что он явился из антиутопичного будущего.""",
+        },
+    )
+    return render(request, "steins-gate_page.html", {"anime": anime})
 
-def steins_gate_zero_page(request: HttpRequest) -> HttpResponse:
-    return render(request, "steins-gate-zero_page.html")
+def steins_gate_zero_page(request):
+    anime, created = AnimeDescription.objects.get_or_create(
+        name="Steins;Gate 0",
+        defaults={
+            "appearing": "2018 весна",
+            "type": "ТВ (24 эп.), 25 мин.",
+            "genres": "Триллер, Фантастика, Научная фантастика, Тайна, Психологический хоррор, Психология, Романтика",
+            "description": """Альтернативная концовка Врат Штейна
+        в которой эгоцентричный безумный ученый Окабе Ринтаро изо всех сил старается оправиться от неудачной попытки спасти жизнь Курису Макисе.
+        Стараясь забыть прошлое, Окабе отказывается от своего альтер-эго. И когда, казалось бы, всё наладилось, он снова сталкивается со своим прошлым.
+        Он знакомится с девушкой, которая представилась знакомой Курису.
+        От неё Окабе узнает, что в данный момент проходит испытание устройства, которое способно воссоздавать характер и личность человека по воспоминаниям.
+        Начиная тестирование он и не предполагал, что воссоздание Курису принесет столько мучений и новых неожиданных последствий..."""
+        }
+    )
+    return render(request, "steins-gate-zero_page.html", {"anime": anime})
