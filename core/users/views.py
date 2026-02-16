8f8e1f6e6dd4137a17cf17884618d62c7d9ca9e3
mailor <Vvorlovv7799@gmail.com>
2026-02-17 00:34:08 +0200

    Update views.py

--- core/users/views.py
+++ core/users/views.py
@@ -30,7 +30,7 @@ def register_view(request):
             return redirect('email_verification')
 
     else:
-        form = RegisterModel()
+        form = RegisterForm()
         logger.info("Registration page opened", extra={'username': request.user.username if request.user.is_authenticated else None})
     return render(request, 'users/register.html', {"form": form})
 
