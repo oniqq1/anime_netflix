bbcb4923fc112caaeb6aae8a2e6e04e6891414bb
oinqq <insolencem1.1@gmail.com>
2026-02-20 18:28:29 +0200

    deploy plsds

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-htpejgnjs9#m)r3%d5ut2s!uqeuyikli0=j0#u2u$%&15806g+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
