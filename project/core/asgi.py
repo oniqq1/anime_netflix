9346de5d8c03da12f4581541983b85e317671abb
oinqq <insolencem1.1@gmail.com>
2026-02-20 20:20:36 +0200

    ЗДІЛЩІФЛ-ЩВДІФЛЩАО0ЩФВАФІВ

--- project/core/asgi.py
+++ project/core/asgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
-import os
 
+
+import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
