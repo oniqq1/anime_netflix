a12f75a548290f33b348b7cb63b62c02c71863d0
oinqq <insolencem1.1@gmail.com>
2026-02-20 20:03:28 +0200

    здыыы

--- project/core/settings.py
+++ project/core/settings.py
@@ -197,7 +197,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
